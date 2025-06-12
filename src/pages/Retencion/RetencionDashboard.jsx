5ac3ed8f85d9cc2ae4f9322bd4d0a280ed2cc602
miky-gas <niltaurion0@gmail.com>
2025-06-12 10:17:30 +0200

    Incoprorar selector fecha incorporación

--- src/pages/Retencion/RetencionDashboard.jsx
+++ src/pages/Retencion/RetencionDashboard.jsx
@@ -14,8 +14,11 @@ const RetencionDashboard = (props) => {
                     <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12 mzh-xl-100 mzh-lg-100 mzh-md-50 mzh-sm-100 mzh-xs-100 mzh-100 box_object">
                         <GenericQlikObject aplication={props.aplication} qlikObjectID={'Pjjse'} visual="" onObjectLoaded={ObjectLoaded} location={ROUTER.globals.current.name} router={ROUTER}></GenericQlikObject>
                     </div>
-                    <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12 mzh-xl-100 mzh-lg-100 mzh-md-50 mzh-sm-100 mzh-xs-100 mzh-100 box_object">
+                    <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12 mzh-xl-30 mzh-lg-30 mzh-md-30 mzh-sm-30 mzh-xs-30 mzh-30 box_object">
                         <GenericQlikObject aplication={props.aplication} qlikObjectID={'JFALTK'} visual="" onObjectLoaded={ObjectLoaded} location={ROUTER.globals.current.name} router={ROUTER}></GenericQlikObject>
+                    </div>
+                    <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12 mzh-xl-70 mzh-lg-70 mzh-md-70 mzh-sm-70 mzh-xs-70 mzh-70 box_object">
+                        <GenericQlikObject aplication={props.aplication} qlikObjectID={'fAZQJz'} visual="" onObjectLoaded={ObjectLoaded} location={ROUTER.globals.current.name} router={ROUTER}></GenericQlikObject>
                     </div>
                     <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12 mzh-xl-100 mzh-lg-100 mzh-md-50 mzh-sm-100 mzh-xs-100 mzh-100 box_object">
                         <GenericQlikObject aplication={props.aplication} qlikObjectID={'YqzWQq'} visual="" onObjectLoaded={ObjectLoaded} location={ROUTER.globals.current.name} router={ROUTER}></GenericQlikObject>
