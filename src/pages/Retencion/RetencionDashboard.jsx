8bfa737c7f6dc0dbe1ea9a9b02723e9c1d91fc6b
miky-gas <niltaurion0@gmail.com>
2025-06-26 12:01:41 +0200

    Retencion - desde hasta y borrar selecciones

--- src/pages/Retencion/RetencionDashboard.jsx
+++ src/pages/Retencion/RetencionDashboard.jsx
@@ -16,12 +16,17 @@ const RetencionDashboard = (props) => {
                     </div>
                     
                     <div className=" flex-component flex-row col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 mzh-xl-100 mzh-lg-100 mzh-md-100 mzh-sm-100 mzh-xs-100 mzh-100 box_object" >
-                        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-4 col-xs-4 mzh-xl-80 mzh-lg-80 mzh-md-80 mzh-sm-80 mzh-xs-80 mzh-80 box_object">
+                        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-4 col-xs-4 mzh-xl-25 mzh-lg-25 mzh-md-25 mzh-sm-25 mzh-xs-25 mzh-25 box_object">
                                     <GenericQlikObject aplication={props.aplication} qlikObjectID={'fAZQJz'} visual="" onObjectLoaded={ObjectLoaded} location={ROUTER.globals.current.name} router={ROUTER}></GenericQlikObject>
                         </div>
-
+                        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-4 col-xs-4 mzh-xl-25 mzh-lg-25 mzh-md-25 mzh-sm-25 mzh-xs-25 mzh-25 box_object">
+                                    <GenericQlikObject aplication={props.aplication} qlikObjectID={'XjmDP'} visual="" onObjectLoaded={ObjectLoaded} location={ROUTER.globals.current.name} router={ROUTER}></GenericQlikObject>
+                        </div>
                         <div className="col-xl-12 col-lg-12 col-md-12 col-sm-4 col-xs-4 mzh-xl-20 mzh-lg-20 mzh-md-20 mzh-sm-20 mzh-xs-20 mzh-20 box_object">
                                     <GenericQlikObject aplication={props.aplication} qlikObjectID={'JFALTK'} visual="" onObjectLoaded={ObjectLoaded} location={ROUTER.globals.current.name} router={ROUTER}></GenericQlikObject>
+                        </div>
+                        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-4 col-xs-4 mzh-xl-20 mzh-lg-20 mzh-md-20 mzh-sm-20 mzh-xs-20 mzh-20 box_object">
+                                    <GenericQlikObject aplication={props.aplication} qlikObjectID={'DAMa'} visual="" onObjectLoaded={ObjectLoaded} location={ROUTER.globals.current.name} router={ROUTER}></GenericQlikObject>
                         </div>
                     </div>
                     <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 mzh-xl-100 mzh-lg-100 mzh-md-100 mzh-sm-100 mzh-xs-100 mzh-100 box_object">
